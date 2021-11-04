51cf56b93d7f194b4b3a16796c317a0f3e9ae99e
mkamilaa <kamila.mqa@gmail.com>
2021-11-04 19:26:32 +0900

    FBV/Serializers/CBV

---
+++ cbvCourse/cbvCourseApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    ratings = models.DecimalField(max_digits = 10, decimal_places = 3)
+
+    def __str__(self):
+        return self.id + self.name + self.description + self.ratings
